c6907534128fcc2cb9c7934ec5d8d62df2ef475c
Sagiv Rada <InvaGirSP@gmail.com>
2018-02-27 12:57:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
     $(document).ready(function(){
         $(window).bind('beforeunload', function() {
             window.open("third.html", "_blank","width=1200,height-628");
-            return 'Are you sure you want to leave?';
         });
     });
 })();
